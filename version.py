af3c9396d5c166f9084e1c4eb7f2cd99e51ef12c
Robin Visser <robinvisser92@live.nl>
2022-02-16 17:07:09 +0100

    Update version.py

--- version.py
+++ version.py
@@ -2,13 +2,20 @@ import win32com.client
 import pytz
 import datetime
 
+C
 
 ids = [i for i in range(100)]
-
+installations = []
 for i in ids:
     try:
-        OneNote_AppID = win32com.client.Dispatch(f'OneNote.Application.{i}')
-        print(OneNote_AppID)
+        OneNote_Find_AppID = win32com.client.Dispatch(f'OneNote.Application.{i}')
+        OneNote = OneNote_Find_AppID
+        installations.append(OneNote)
     except:
         pass
 
+print(installations)
+
+onObj = win32com.client.gencache.EnsureDispatch('OneNote.Application.12')
+result = onObj.GetHierarchy("",win32com.client.constants.hsNotebooks)
+print(result)
